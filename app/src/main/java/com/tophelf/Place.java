10c80b4fab5eba7c991a03df2726ddca0cb977c3
fikret kaya <fikret_kya@hotmail.com>
2016-05-03 12:51:42 +0300

    asda

---
+++ app/src/main/java/com/tophelf/Place.java
@@ -0,0 +1,48 @@
+package com.tophelf;
+
+/**
+ * Created by FKRT on 27.04.2016.
+ */
+public class Place {
+
+    private String name, id, info, loc;
+
+    public Place(String name, String id, String info, String loc) {
+        this.name = name;
+        this.id = id;
+        this.info = info;
+        this.loc = loc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+}
